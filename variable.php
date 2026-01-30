b1f80d5297840078f4caedb8a9e94be4ac8b6a9e
Madhusudan Dash <madhusudandash98@gmail.com>
2026-01-30 20:15:05 +0530

    variable

---
+++ variable.php
@@ -0,0 +1,12 @@
+<?php
+$name = "Madhusudan dash" . "<br>";  // variable declaration
+echo $name;
+$num = 10 ;
+
+$string = "1500"  . "<br>"; 
+echo $num . "<br>";
+echo $string ;
+var_dump($num)  ;
+echo "<br>";
+var_dump($string); //
+?>
