33e5421313be326fac566c6774a6f28d87dd8150
Madhusudan Dash <madhusudandash98@gmail.com>
2026-01-31 15:36:13 +0530

    constant

---
+++ constant.php
@@ -0,0 +1,17 @@
+<?php
+ const NAME = "madhu";
+ echo NAME ;
+echo "<br>";
+$n = 50;
+echo $n;
+echo"<br>";
+const MADHU = 27 ;
+echo MADHU ;
+$n = 505;
+echo "<br>";
+echo $n ;
+echo "<br>";
+const _MAD8HU = 2852 ;  // CONSTANT redefine not allowed
+echo _MAD8HU ;
+
+?>
--- variable.php
+++ variable.php
@@ -3,10 +3,10 @@ $name = "Madhusudan dash" . "<br>";  // variable declaration
 echo $name;
 $num = 10 ;
 
-$string = "1500"  . "<br>"; 
+$string = "1500" .  "<br>"; // double quotations used
 echo $num . "<br>";
 echo $string ;
 var_dump($num)  ;
 echo "<br>";
-var_dump($string); //
+var_dump($string); //check variable contains
 ?>
