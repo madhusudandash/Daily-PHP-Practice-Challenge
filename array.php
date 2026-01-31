099ca534db861147d1ea5fd16e0d59ad778902a0
Madhusudan Dash <madhusudandash98@gmail.com>
2026-01-31 15:51:12 +0530

    array

---
+++ array.php
@@ -0,0 +1,16 @@
+<?php
+$arr = ["madhu","50"];
+// echo $arr; // echo cannot print array directly 
+print_r($arr);
+echo "<br>";
+echo $arr[0];
+echo "<br>" ;
+echo $arr[1];
+echo "<br>";
+echo var_dump ($arr); 
+echo "<br>";
+$msd = array("madhu","dash",1551, null,null);
+print_r($msd);
+echo "<br>";
+var_dump($msd);
+?>
